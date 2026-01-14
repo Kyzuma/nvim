a951c8527f3d90fea88afb1ddaeadfd530d22901
Kyzuma <leekayong@gmail.com>
2026-01-14 16:23:24 +0800

    feat: added toggleterm

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,35 +1,75 @@
--- telescope
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { desc = 'Telescope live grep'})
-
--- neo-tree
-vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>')
-
--- barbar
-local map = vim.api.nvim_set_keymap
+-- Helper
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Move to previous/next
+-- =========================================================
+-- Clipboard
+-- =========================================================
+map({ "n", "v" }, "<leader>y", '"+y', opts)  -- Copy to system clipboard
+map({ "n", "v" }, "<leader>p", '"+p', opts)  -- Paste from system clipboard
+
+-- =========================================================
+-- Telescope
+-- =========================================================
+map('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Telescope: find files' })
+map('n', '<leader>fg', ':Telescope live_grep<CR>', { desc = 'Telescope: live grep' })
+
+-- =========================================================
+-- Neo-tree
+-- =========================================================
+map('n', '<leader>e', ':Neotree filesystem reveal left<CR>', opts)
+
+-- =========================================================
+-- Barbar (Buffer management)
+-- =========================================================
+-- Move to previous/next buffer
 map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
 
--- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+-- Go to buffer in position 1-9 / last buffer
+for i = 1, 9 do
+  map('n', '<A-' .. i .. '>', '<Cmd>BufferGoto ' .. i .. '<CR>', opts)
+end
 map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
 
--- Close buffer
+-- Close / pin buffers
 map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
-
--- Pin/unpin buffer
 map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
 
+-- =========================================================
 -- LazyGit
+-- =========================================================
 map('n', '<leader>lg', '<Cmd>LazyGit<CR>', opts)
+
+-- =========================================================
+-- ToggleTerm / Terminal
+-- =========================================================
+local terminal_count = 0
+
+-- Spawn a new floating terminal
+local function new_float_term()
+  terminal_count = terminal_count + 1
+  vim.cmd("ToggleTerm " .. terminal_count .. " direction=float")
+end
+
+-- Toggle / focus last terminal
+map("n", "<leader>tf", "<cmd>ToggleTerm<CR>", opts)
+
+-- Spawn a brand new floating terminal
+map("n", "<leader>tt", new_float_term, opts)
+
+-- Terminal buffer selection and naming
+map("n", "<leader>ts", "<cmd>TermSelect<CR>", opts)
+map("n", "<leader>tn", "<cmd>ToggleTermSetName<CR>", opts)
+
+-- Terminal navigation / insert mode
+map("t", "<Esc>", [[<C-\><C-n>]], opts)         -- Exit terminal input mode
+map("n", "i", "<cmd>startinsert<CR>", opts)     -- Enter insert mode
+map("t", "<leader>q", [[<C-\><C-n><cmd>close<CR>]], opts) -- Close terminal window
+
+-- Auto enter insert mode whenever opening a terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.cmd("startinsert")
+  end,
+})
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return{
+  'akinsho/toggleterm.nvim', 
+  version = "*", 
+  opts = {
+    direction=tabs
+    --[[ things you want to change go here]]
+  }
+}
+
